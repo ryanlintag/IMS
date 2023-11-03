a51c97d8a9f4143560c1e7f0a7c5367cb3e5e0e9
Ryan Dela Torre LINTAG <lintagr@who.int>
2023-11-03 22:40:01 +0800

    Added Initial Domain Tests

---
+++ Domain/Abstractions/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Abstractions
+{
+    public abstract class Entity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        protected Entity(Guid id) 
+        {
+            this.Id = id;
+        }
+        protected Entity() { }
+        public Guid Id { get; protected set; }
+
+        public List<IDomainEvent> DomainEvents { get {  return _domainEvents; } }
+
+        protected void Raise(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+    }
+}
---
+++ Domain/Users/User.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Users
+{
+    public class User : Entity
+    {
+        public User(Guid id) : base(id)
+        {
+        }
+        private User() { }
+    }
+}
