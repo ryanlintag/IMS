258de1eeba5cdf5107acac28fac7ea43cc50902c
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2023-11-11 02:51:32 +0800

    Results - Error base handler

---
+++ Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Abstractions
+{
+    public record DomainError(string Code, string? Description = null)
+    {
+        public static readonly DomainError None = new(string.Empty);
+        public static implicit operator Result(DomainError error) => Result.Failure(error);
+    }
+}
